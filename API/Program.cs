78ce5860e91cbe04e7ee1250eff9d2333a8867ad
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-02-19 16:03:41 +0200

    Added seed class to persistance, added automatic seeding to main

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistance;
+using Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,9 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            //Customized the main method to create db if not already created
+            //Customized the main method to create and seed db if not already created
             var host = CreateHostBuilder(args).Build();
 
             using var scope = host.Services.CreateScope();
@@ -26,7 +27,8 @@ namespace API
             try
             {
                 var context = services.GetRequiredService<DataContext>();
-                context.Database.Migrate();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context);
             }
             catch (Exception ex)
             {
@@ -34,7 +36,7 @@ namespace API
                 logger.LogError(ex, "An error occured during migration");
             }
 
-            host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
