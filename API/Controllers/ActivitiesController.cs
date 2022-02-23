4e90d150e16966e11080638f51a026dcfd983ce4
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-02-23 14:24:54 +0200

    Moved mediator to BaseApiController as a protected field

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,7 +1,6 @@
 ﻿using API.Controllers.Base;
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,17 +10,10 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
--- API/Controllers/Base/BaseApiController.cs
+++ API/Controllers/Base/BaseApiController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers.Base
 {
@@ -6,6 +8,7 @@ namespace API.Controllers.Base
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
