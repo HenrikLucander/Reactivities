531cc1d3cddb024c2df22ddbbd81480e8ff1c992
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-02-23 14:58:33 +0200

    Added mapping to Edit handler

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,10 +1,7 @@
-﻿using Domain;
+﻿using AutoMapper;
+using Domain;
 using MediatR;
 using Persistance;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,17 +17,19 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
-                activity.Title = request.Activity.Title ?? activity.Title;
+                _mapper.Map(request.Activity, activity);
 
                 await _context.SaveChangesAsync();
 
