7bcb754c27d3575b984b3d3306a36fee6673132a
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-03-22 08:46:06 +0200

    implemented form logic

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,4 +1,4 @@
-﻿import React from 'react'
+﻿import React, { ChangeEvent, FormEvent, useState } from 'react'
 import { Activity } from '../../../app/models/activity'
 
 interface Props {
@@ -6,27 +6,50 @@ interface Props {
     closeForm: () => void;
 }
 
-export default function ActivityForm({activity, closeForm}: Props) {
+export default function ActivityForm({ activity: selectedActivity, closeForm }: Props) {
+
+    const initialState = selectedActivity ?? {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: '',
+        city: '',
+        venue: ''
+    }
+
+    const [activity, setActivity] = useState(initialState);
+
+    function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+        console.log(activity);
+    }
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const { name, value } = event.target;
+        setActivity({...activity, [name]: value})
+    }
+
     return (
         <div className="ui segment clearing">
-            <form className="ui form">
+            <form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
                 <div className="field">
-                    <input type="text" name="Title" placeholder="Title" />
+                    <input type="text" placeholder="Title" name="title" value={activity.title} onChange={handleInputChange} />
                 </div>
                 <div className="field">
-                    <textarea name="Description" placeholder="Description" rows={1}></textarea>
+                    <textarea placeholder="Description" rows={1} name="description" value={activity.description} onChange={handleInputChange}></textarea>
                 </div>
                 <div className="field">
-                    <input type="text" name="Category" placeholder="Category" />
+                    <input type="text" placeholder="Category" name="category" value={activity.category} onChange={handleInputChange}/>
                 </div>
                 <div className="field">
-                    <input type="text" name="Date" placeholder="Date" />
+                    <input type="text" placeholder="Date" name="date" value={activity.date} onChange={handleInputChange}/>
                 </div>
                 <div className="field">
-                    <input type="text" name="City" placeholder="City" />
+                    <input type="text" placeholder="City" name="city" value={activity.city} onChange={handleInputChange}/>
                 </div>
                 <div className="field">
-                    <input type="text" name="Venue" placeholder="Venue" />
+                    <input type="text" placeholder="Venue" name="venue" value={activity.venue} onChange={handleInputChange}/>
                 </div>
                 <button className="ui button right floated positive" type="submit">Submit</button>
                 <button onClick={closeForm} className="ui button right floated" type="button">Cancel</button>              
