f54aa16d580937bc7a1181135fe4494c19b21143
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-03-22 14:49:57 +0200

    Added loading indicator and fake delay

---
+++ client-app/src/app/layouts/LoadingComponent.tsx
@@ -0,0 +1,15 @@
+﻿
+interface Props {
+    inverted?: boolean;
+    content?: string;
+}
+
+export default function LoadingComponent({ inverted = true, content = 'Loading...' }: Props) {
+    return (
+        <div className="ui active inverted dimmer">
+            <div className="ui text loader">
+                {content}
+            </div>
+        </div>
+    )
+}
